e1ecea016ab99c5f90e9c8e2ae404d07aff25f06
starci183 <cuongnvtse160875@gmail.com>
2024-11-20 14:54:33 +0700

    4

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
     // If you aren't using GitHub pages, you don't need these.
     organizationName: "starci-lab", // Usually your GitHub org/user name.
     projectName: "cifarm-docs", // Usually your repo name.
+    trailingSlash: false,
 
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
